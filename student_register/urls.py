3fb2cdc46aebce59eda907919f8d6a629efe2d07
MSKose <saitmustafa1998@gmail.com>
2022-09-04 22:13:13 +0200

    added a view to handle both update and create

--- student_register/urls.py
+++ student_register/urls.py
@@ -4,7 +4,7 @@ from .views import student_add_update, student_list, student_delete
 urlpatterns = [
     # path('', home, name='home'),
     path('', student_add_update, name="form"),
-    path('<int:pk>/', student_add_update, name="update"),
+    path('<int:id>/', student_add_update, name="update"),
     path('list/', student_list, name="list"),
     # path('delete/<int:id>/', student_delete, name="delete"),
 ]
--- student_register/views.py
+++ student_register/views.py
@@ -7,12 +7,19 @@ from .forms import PersonForm
 
 
 
-def student_add_update(request, pk = -7):
-    if pk != -7:
-        todo = Student.objects.get(id=pk)
+def student_add_update(request, id = -7): # equating id to -7 and then checking with an if-else whether student_add_update should do an update or create. If id is not equal to -7 it means it is an update
+    if id != -7:
+        todo = Student.objects.get(id=id)
         form = PersonForm(instance=todo)
 
+        if request.method == "POST":
+            form = PersonForm(request.POST, instance=todo)
+            if form.is_valid():
+                form.save()
+                return redirect("list")
+
         context = {
+            'todo': todo,
             'form' : form,
         }
         return render(request, 'student_register/student_form.html', context)
@@ -34,6 +41,29 @@ def student_add_update(request, pk = -7):
         }
 
         return render(request, 'student_register/student_form.html', context)
+
+
+# def student_add_update(request, pk):
+
+#     todo = Student.objects.get(id=pk)
+
+#     try: # get pre-populated form with model instance data (for update)
+#         form = PersonForm(instance=todo.id) 
+#         print('here', form)
+#     except: # If it doesn't exist, show an empty form (for create)
+#         form = PersonForm(request.POST or None)
+#         print('here2', form)
+
+#     if request.method == 'POST':
+#         try: # Do the same as above
+#             form = PersonForm(instance=todo.id)
+#         except: # Same as above
+#             form = PersonForm(request.POST or None)
+#         if form.is_valid():
+#             form.save()
+
+
+#     return render(request, 'student_register/student_form.html', {'form':form})
 
 
 
